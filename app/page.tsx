fb05eea74ecb34f263e01d1bd87c6e13d423f712
sachindhote05 <sachindhote7905@gmail.com>
2026-03-11 11:20:24 +0530

    ui

--- app/page.tsx
+++ app/page.tsx
@@ -78,8 +78,8 @@ useEffect(() => {
 </h1>
 
 <h2 className="text-3xl md:text-4xl font-extrabold flex items-baseline gap-3">
-<span className="text-white">Elevating elevating
-  
+<span className="text-white">Elevating
+
 </span>
 
   <span className="relative h-[50px] overflow-hidden">
