48b91c227251d32def19101cdceb8d951b43c810
sachindhote05 <sachindhote7905@gmail.com>
2026-03-13 15:30:01 +0530

    fix media page

---
+++ app/resources/media/page.tsx
@@ -0,0 +1,7 @@
+export default function MediaPage() {
+  return (
+    <div>
+      <h1>Media Page</h1>
+    </div>
+  )
+}
---
+++ app/resources/newsletters/page.tsx
@@ -0,0 +1,7 @@
+export default function MediaPage() {
+  return (
+    <div>
+      <h1>News Letter</h1>
+    </div>
+  )
+}
